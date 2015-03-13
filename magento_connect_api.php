229df4a379c9575429dc8c035739435e432ad9ff
Alejandro Caldarone <alejandro.caldarone@gmail.com>
2015-03-13 12:40:07 -0300

    Test Magento Api Soap

--- magento_connect_api.php
+++ magento_connect_api.php
@@ -14,15 +14,22 @@
 		// Login SOAP
 		$session = $client->login('api_temp', 'api_temp_123');
 		
+		// Store
+		$storeList = $client->call($session, 'store.list');
+		$store = $client->call($session, 'store.info', '1');
+
 		// Pedimos un listado de las ordenes actualizadas entre un rango de fechas.
 		$list_update_order = $client->call(
 			$session,
 			'sales_order.list',
 			array(
 				'filter' => array(
+					//'status' => array('pending'),
+					'store_id' => array('1'),
+					//'store_name' => array('like' => '%English'),
 					'updated_at' => array(
-						'from' => '2015-03-02 16:45:30',
-						'to' => '2015-03-04 16:45:30'
+						'from' => '2012-03-02 16:45:30',
+						'to' => '2015-03-13 16:45:30'
 					)
 				)
 			)
@@ -45,7 +52,7 @@
 		$serialize_list_order = serialize($list_order);
 		
 		echo '<pre> <p>ORDER:</p>';
-		print_r($list_order);
+		print_r($storeList);
 		echo '</pre>';
 	} catch (Exception $e) {
 		echo '<pre> <p>Exception:</p>';
